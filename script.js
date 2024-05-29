2553444fa7598a2a3a40456bfc81a995317fe6d7
Alencar-Bianca <73860563+Alencar-Bianca@users.noreply.github.com>
2024-05-28 21:07:21 -0300

    finish validation

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ let isCheckedRaio = false
 const submit = document.querySelector('#button_submit');
 const form = document.getElementById('form');
 
+const nameSpan = document.querySelector('.firstName');
+const lastNameSpan = document.querySelector('.lastName');
+const emailSpan = document.querySelector('.email');
+const messageSpan = document.querySelector('.message');
+
+const checkboxSpan = document.querySelector('.checkbox');
+const radioButtonsSpan = document.querySelector('.radio')
+
 submit.addEventListener('click', (e) => {
   e.preventDefault()
   radioButtons.forEach(radioButton => {
@@ -18,29 +26,62 @@ submit.addEventListener('click', (e) => {
       }
   });
 
-  if(name.value == '') {
-    alert('nome vazio')
-  }
-  if(lastName.value == '') {
-    alert('sobrenome vazio')
-  }
-  if(email.value == '') {
-    alert('email vazio')
-  }
-  if(message.value == '') {
-    alert('email vazio')
-  }
-  if(!checkbox.checked) {
-    alert('checkbox vazio')
-  }
-  if(!isCheckedRaio) {
-    alert('isCheckedRaio vazio')
-  }
 
-  if(name.value !== '' && lastName.value !== '' && email.value !== '' && message.value !== '' && checkbox.checked && isCheckedRaio) {
+  if (name.value === '') {
+    name.classList.add("error");
+    nameSpan.classList.remove("d-none");
+   } else {
+    name.classList.remove("error");
+    nameSpan.classList.add("d-none");
+   }
+
+  if (lastName.value === '') {
+    lastName.classList.add("error");
+    lastNameSpan.classList.remove("d-none");
+   } else {
+    lastName.classList.remove("error");
+    lastNameSpan.classList.add("d-none");
+   }
+
+  if (email.value === '') {
+    email.classList.add("error");
+    emailSpan.classList.remove("d-none");
+   } else {
+    email.classList.remove("error");
+    emailSpan.classList.add("d-none");
+   }
+
+  if (message.value === '') {
+    message.classList.add("error");
+    messageSpan.classList.remove("d-none");
+
+   } else {
+    message.classList.remove("error");
+    messageSpan.classList.add("d-none");
+   }
+
+  if (!checkbox.checked) {
+    checkboxSpan.classList.remove("d-none");
+        
+   } else {
+    checkboxSpan.classList.add("d-none");
+   }
+
+  if (!isCheckedRaio) {
+    radioButtonsSpan.classList.remove("d-none");
+        
+   } else {
+    radioButtonsSpan.classList.add("d-none");
+   }
+
+   if(name.value !== '' && lastName.value !== '' && email.value !== '' && message.value !== '' && checkbox.checked && isCheckedRaio) {
     form.submit();
-  }
- 
- 
+   }
   
 });
+
+function alertError(message, el) {
+
+  el.classList.add("error");
+
+}
